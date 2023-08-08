0e5fdfd0ee0b9be10b6a2d43640e53a6a30cb97e
Dhakarpawan <133591759+Dhakarpawan@users.noreply.github.com>
2023-08-08 18:33:00 +0530

    Pawan

---
+++ MoneyHeist/lib/main.dart
@@ -0,0 +1,27 @@
+import 'NavBarPage.dart';
+import 'provider_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light));
+  runApp(ChangeNotifierProvider<ListProvider>(
+    create: (_) => ListProvider(),
+    child: const MainApp(),
+  ));
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: NavBarPage(),
+    );
+  }
+}
